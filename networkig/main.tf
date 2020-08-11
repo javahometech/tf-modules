b0471712fcc2f7207b80e826594b4227a84bb9bb
javahometech <kammana.hari@gmail.com>
2020-08-11 09:54:33 +0530

    Update main.tf

--- networkig/main.tf
+++ networkig/main.tf
@@ -90,7 +90,7 @@ resource "aws_route_table_association" "b" {
 
 # Create NAT instance in public subnet
 resource "aws_instance" "nat" {
-  ami                         = lookup(var.nat_amis, var.region)
+  ami                         = lookup(var.nat_amis, var.stack_region)
   instance_type               = "t2.micro"
   subnet_id                   = local.pub_sub_ids[0]
   associate_public_ip_address = true
